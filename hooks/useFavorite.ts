7f389eaec512f5e193e65d6eb81ae5eeb9d2860e
joaoricardoM <joao_pia_mds@hotmail.com>
2023-05-30 16:59:36 -0300

    feat: create API favorites

---
+++ hooks/useFavorite.ts
@@ -0,0 +1,15 @@
+import useSWR from "swr";
+import fetcher from "@/lib/fetcher";
+
+const useFavorites = () => {
+    const {
+        data,
+        error,
+        isLoading,
+        mutate
+    } = useSWR('api/favorites', fetcher, {
+        revalidateIfState: false,
+        revalidateOnFocus: false,
+        revalidadeOnReconnect: false,
+    });
+}
