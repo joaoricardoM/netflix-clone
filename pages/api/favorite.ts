395ca905d85492d6bb1887175a9469c81e730c53
joaoricardoM <joao_pia_mds@hotmail.com>
2023-05-31 16:40:37 -0300

    fix: error fixed

--- pages/api/favorite.ts
+++ pages/api/favorite.ts
@@ -13,12 +13,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
             const existingMovie = await prismadb.movie.findUnique({
                 where: {
-                    id: movieId
+                    id: movieId,
                 }
-            })
+            });
 
             if (!existingMovie) {
-                throw new Error('Invalid ID')
+                throw new Error('Invalid ID');
             }
 
             const user = await prismadb.user.update({
@@ -30,40 +30,44 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                         push: movieId
                     }
                 }
-            })
+            });
 
-            return res.status(200).json(user)
+            return res.status(200).json(user);
         }
 
         if (req.method === 'DELETE') {
             const { currentUser } = await serverAuth(req, res);
+
             const { movieId } = req.body;
-            const existingMovieId = await prismadb.movie.findUnique({
+
+            const existingMovie = await prismadb.movie.findUnique({
                 where: {
                     id: movieId,
                 }
-            })
-            if (existingMovieId) {
-                throw new Error('invalid ID')
+            });
+
+            if (!existingMovie) {
+                throw new Error('Invalid ID');
             }
 
-            const updateFavoriteIds = without(currentUser.favoriteIds, movieId);
+            const updatedFavoriteIds = without(currentUser.favoriteIds, movieId);
+
             const updatedUser = await prismadb.user.update({
                 where: {
                     email: currentUser.email || '',
                 },
                 data: {
-                    favoriteIds: updateFavoriteIds
+                    favoriteIds: updatedFavoriteIds,
                 }
-            })
+            });
 
             return res.status(200).json(updatedUser);
         }
 
-        return res.status(405).end()
+        return res.status(405).end();
     } catch (error) {
-        console.log(error)
-        return res.status(400).end()
-    }
+        console.log(error);
 
+        return res.status(500).end();
+    }
 }
