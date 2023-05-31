a7e2045fc49f1e54998031470f4ceb7cbac2786c
joaoricardoM <joao_pia_mds@hotmail.com>
2023-05-31 15:41:04 -0300

    fix: fix auth for new version

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -1,24 +1,27 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
 
-import prismadb from "../lib/prismadb";
+import prismadb from '@/lib/prismadb';
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getSession({ req });
+  const session = await getServerSession(req, res, authOptions);
 
   if (!session?.user?.email) {
-    throw new Error("Not signed in");
+    throw new Error('Not signed in');
   }
 
   const currentUser = await prismadb.user.findUnique({
-    where: { email: session.user.email },
+    where: {
+      email: session.user.email,
+    }
   });
 
   if (!currentUser) {
-    throw new Error("Not signed in");
+    throw new Error('Not signed in');
   }
 
   return { currentUser };
-};
+}
 
 export default serverAuth;
